71fed255115b975d8d178f0eed897b3d5bbc50c9
Matan Balestra <matan.balestra@gmail.com>
2024-12-15 19:24:43 +0200

    bigger hero

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -61,7 +61,7 @@ const HeroSection = () => {
           </div>
         </div>
 
-        <div className="max-w-lg mx-auto">
+        <div className="max-w-xl mx-auto">
           <h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide">Building Exceptional User Experiences</h1>
           <p className="mt-4 text-center text-white/60 md:text-lg">I specialize in transforming designs into functional, high-performing web applications. Let&apos;s discuss your next project.</p>
         </div>
