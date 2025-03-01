97bc4b19699cddad08973dcb323a03199defd707
Matan Balestra <matan.balestra@gmail.com>
2025-03-01 15:34:05 +0200

    change name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,19 @@
 import type { Metadata } from "next";
-import { Inter, Calistoga } from 'next/font/google';
+import { Inter, Calistoga } from "next/font/google";
 import "./globals.css";
 
 import { twMerge } from "tailwind-merge";
 
-const inter = Inter({ subsets: ['latin'], variable: '--font-sans'});
-const calistoga = Calistoga({ subsets: ['latin'], variable: '--font-serif', weight: ["400"]});
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+const calistoga = Calistoga({
+  subsets: ["latin"],
+  variable: "--font-serif",
+  weight: ["400"],
+});
 
 export const metadata: Metadata = {
-  title: "Matan Balestra",
-  description: "Matan Balestra's personal website",
+  title: "Matan Bar",
+  description: "Matan Bar's personal website",
 };
 
 export default function RootLayout({
@@ -21,7 +25,15 @@ export default function RootLayout({
     <html lang="en">
       <meta charSet="utf-8" />
       <link rel="icon" href="/memoji-computer.png" type="image/x-icon" />
-      <body className={twMerge(inter.variable, calistoga.variable, "bg-gray-900 text-white antialiased font-sans")}>{children}</body>
+      <body
+        className={twMerge(
+          inter.variable,
+          calistoga.variable,
+          "bg-gray-900 text-white antialiased font-sans"
+        )}
+      >
+        {children}
+      </body>
     </html>
-  );  
+  );
 }
--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,15 +1,15 @@
-import ArrowUpRightIcon from '@/assets/icons/arrow-up-right.svg';
+import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
 
 const footerLinks = [
   {
-    title: 'LinkedIn',
-    href: 'https://www.linkedin.com/in/matanbalestra'
+    title: "LinkedIn",
+    href: "https://www.linkedin.com/in/matanbar",
   },
   {
-    title: 'Github',
-    href: 'https://github.com/balestramatan'
-  }
-]
+    title: "Github",
+    href: "https://github.com/matanbbar",
+  },
+];
 
 const Footer = () => {
   return (
@@ -18,15 +18,18 @@ const Footer = () => {
       <div className="container">
         <div className="border-t border-white/15 py-6 text-sm flex flex-col md:flex-row md:justify-between items-center gap-8">
           <div className="text-white/40">&copy; 2024. All rights reserved.</div>
-          <nav className='flex flex-col md:flex-row items-center gap-8'>
-            {
-              footerLinks.map((link) => (
-                <a key={link.title} target='_blank' href={link.href} className='inline-flex items-center gap-1.5'>
-                  <span className='font-semibold'>{link.title}</span>
-                  <ArrowUpRightIcon className="size-4" />
-                </a>
-              ))
-            }
+          <nav className="flex flex-col md:flex-row items-center gap-8">
+            {footerLinks.map((link) => (
+              <a
+                key={link.title}
+                target="_blank"
+                href={link.href}
+                className="inline-flex items-center gap-1.5"
+              >
+                <span className="font-semibold">{link.title}</span>
+                <ArrowUpRightIcon className="size-4" />
+              </a>
+            ))}
           </nav>
         </div>
       </div>
